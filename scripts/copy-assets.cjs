757097bbae026bcee27cda7683c0a6751e09b3a0
bendera <szoftverhiba@gmail.com>
2023-10-27 00:38:02 +0200

    Copy assets

---
+++ scripts/copy-assets.cjs
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const util = require("util");
+
+cp = util.promisify(fs.copyFile);
+
+const main = async () => {
+  await cp('node_modules/@vsc-elements/elements/dist/bundled.js', 'public/bundled.js');
+  await cp('node_modules/@vsc-elements/elements/custom-elements.json', 'public/custom-elements.json');
+  await cp('node_modules/@vscode/codicons/dist/codicon.css', 'public/codicon.css');
+  await cp('node_modules/@vscode/codicons/dist/codicon.ttf', 'public/codicon.ttf');
+}
+
+main();
