52ff3e3cba0eadaf652ba6f3390d24c251555562
bendera <szoftverhiba@gmail.com>
2023-10-29 02:05:26 +0200

    wip

--- scripts/copy-assets.cjs
+++ scripts/copy-assets.cjs
@@ -5,7 +5,7 @@ cp = util.promisify(fs.copyFile);
 
 const main = async () => {
   await cp('node_modules/@vsc-elements/elements/dist/bundled.js', 'public/bundled.js');
-  await cp('node_modules/@vsc-elements/elements/custom-elements.json', 'public/custom-elements.json');
+  await cp('node_modules/@vsc-elements/elements/custom-elements.json', 'data/custom-elements.json');
   await cp('node_modules/@vscode/codicons/dist/codicon.css', 'public/codicon.css');
   await cp('node_modules/@vscode/codicons/dist/codicon.ttf', 'public/codicon.ttf');
 }
