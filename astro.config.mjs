e90d747e7c89bdc730c86b3eb1b5b27fe6814517
bendera <szoftverhiba@gmail.com>
2023-11-23 00:29:43 +0100

    Enable prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import {defineConfig} from 'astro/config';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeToc from 'rehype-toc';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vsc-elements.github.io',
   output: 'static',
+  prefetch: true,
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
