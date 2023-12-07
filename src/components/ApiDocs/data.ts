d3f33d6af95358145af817bd18f353afe409f48f
bendera <szoftverhiba@gmail.com>
2023-12-07 23:32:33 +0100

    Simplify the layout

---
+++ src/components/ApiDocs/data.ts
@@ -0,0 +1,109 @@
+import type {
+  Attribute,
+  ClassMember,
+  ClassMethod,
+  CssCustomProperty,
+  CustomElement,
+  CustomElementDeclaration,
+  CustomElementField,
+  Event,
+  Package,
+  Slot,
+} from "custom-elements-manifest";
+import cemData from "@data/custom-elements.json";
+
+const memoizedDeclarations: {
+  [key: string]: {
+    tagName?: string;
+    deprecated?: string | boolean;
+    description?: string;
+    members: ClassMember[];
+    publicMembers: ClassMember[];
+    fields: CustomElementField[];
+    methods: ClassMethod[];
+    attributes: Attribute[];
+    events: Event[];
+    slots: Slot[];
+    cssProps: CssCustomProperty[];
+  }[];
+} = {};
+
+type Comparable = ClassMember | CssCustomProperty;
+
+const memberCompare = (a: Comparable, b: Comparable) => {
+  if (a.name > b.name) {
+    return 1;
+  } else if (a.name < b.name) {
+    return -1;
+  } else {
+    return 0;
+  }
+};
+
+const getElementGroup = (name: string, pack: Package) => {
+  const foundModule = pack.modules.find((m) => {
+    const foundCustomElementDef = m.exports?.find(
+      (e) => e.kind === "custom-element-definition" && e.name === name
+    );
+
+    return foundCustomElementDef;
+  });
+
+  return foundModule;
+};
+
+export const getCustomElementDeclarations = (tagName: string) => {
+  if (memoizedDeclarations[tagName]) {
+    return memoizedDeclarations[tagName];
+  }
+
+  const elementGroup = getElementGroup(tagName, cemData as Package);
+
+  const customElementDeclarations = elementGroup?.declarations
+  ? (elementGroup.declarations.filter(
+      (d) => (d as CustomElementDeclaration).customElement
+    ) as CustomElement[])
+  : [];
+
+  const orderedDeclarations = customElementDeclarations.map((declaration) => {
+    const { tagName, deprecated, description } = declaration;
+    const members = declaration.members ?? [];
+    const publicMembers = members.filter((m) =>
+      m.privacy ? m.privacy !== "private" && m.privacy !== "protected" : true
+    );
+    const fields = publicMembers
+      .filter((m) => m.kind === "field")
+      .sort(memberCompare) as CustomElementField[];
+    const methods = publicMembers
+      .filter((m) => m.kind === "method")
+      .sort(memberCompare) as ClassMethod[];
+    const attributes = declaration.attributes
+      ? declaration.attributes.filter((a) => !a.fieldName).sort(memberCompare)
+      : [];
+    const events = declaration.events
+      ? declaration.events.sort(memberCompare)
+      : [];
+    const slots = declaration.slots ? declaration.slots.sort(memberCompare) : [];
+    const cssProps = declaration.cssProperties
+      ? declaration.cssProperties.sort(memberCompare)
+      : [];
+  
+    return {
+      tagName,
+      deprecated,
+      description,
+      members,
+      publicMembers,
+      fields,
+      methods,
+      attributes,
+      events,
+      slots,
+      cssProps,
+    };
+  });
+
+  memoizedDeclarations[tagName] = orderedDeclarations;
+
+  return orderedDeclarations;
+};
--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const apiCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
+    tagName: z.string(),
   }),
 });
 
