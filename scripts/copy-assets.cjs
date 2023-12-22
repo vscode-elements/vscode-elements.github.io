ab4947f4cbceaa68040534dc4dc0e2d4bac22c50
bendera <szoftverhiba@gmail.com>
2023-12-22 11:52:22 +0100

    Rename project in the docs

--- scripts/copy-assets.cjs
+++ scripts/copy-assets.cjs
@@ -4,7 +4,7 @@ const util = require("util");
 cp = util.promisify(fs.copyFile);
 
 const main = async () => {
-  await cp('node_modules/@vsc-elements/elements/custom-elements.json', 'src/data/custom-elements.json');
+  await cp('node_modules/@vscode-elements/elements/custom-elements.json', 'src/data/custom-elements.json');
   await cp('node_modules/@vscode/codicons/dist/codicon.css', 'public/codicon.css');
   await cp('node_modules/@vscode/codicons/dist/codicon.ttf', 'public/codicon.ttf');
 }
