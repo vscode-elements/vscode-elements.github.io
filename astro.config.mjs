6f5e3a8dc7e489c1c80f7c7922211922d84cc792
bendera <szoftverhiba@gmail.com>
2025-01-11 23:58:12 +0100

    Add piwik tracking code

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,17 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
+const analytics = `
+(function(window, document, dataLayerName, id) {
+window[dataLayerName]=window[dataLayerName]||[],window[dataLayerName].push({start:(new Date).getTime(),event:"stg.start"});var scripts=document.getElementsByTagName('script')[0],tags=document.createElement('script');
+function stgCreateCookie(a,b,c){var d="";if(c){var e=new Date;e.setTime(e.getTime()+24*c*60*60*1e3),d="; expires="+e.toUTCString();f="; SameSite=Strict"}document.cookie=a+"="+b+d+f+"; path=/"}
+var isStgDebug=(window.location.href.match("stg_debug")||document.cookie.match("stg_debug"))&&!window.location.href.match("stg_disable_debug");stgCreateCookie("stg_debug",isStgDebug?1:"",isStgDebug?14:-1);
+var qP=[];dataLayerName!=="dataLayer"&&qP.push("data_layer_name="+dataLayerName),isStgDebug&&qP.push("stg_debug");var qPString=qP.length>0?("?"+qP.join("&")):"";
+tags.async=!0,tags.src="https://szoftverhiba.containers.piwik.pro/"+id+".js"+qPString,scripts.parentNode.insertBefore(tags,scripts);
+!function(a,n,i){a[n]=a[n]||{};for(var c=0;c<i.length;c++)!function(i){a[n][i]=a[n][i]||{},a[n][i].api=a[n][i].api||function(){var a=[].slice.call(arguments,0);"string"==typeof a[0]&&window[dataLayerName].push({event:n+"."+i+":"+a[0],parameters:[].slice.call(arguments,1)})}}(i[c])}(window,"ppms",["tm","cm"]);
+})(window, document, 'dataLayer', '5f92a4a8-87f5-418f-ac6f-1817837888c3');
+`;
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://vscode-elements.github.io",
@@ -12,10 +23,19 @@ export default defineConfig({
       social: {
         github: "https://github.com/vscode-elements/elements",
       },
-      favicon: '/favicon.ico',
+      favicon: "/favicon.ico",
       logo: {
         src: "/src/assets/logo.svg",
       },
+      head: [
+        {
+          tag: "script",
+          attrs: {
+            type: "text/javascript",
+          },
+          content: analytics,
+        },
+      ],
       editLink: {
         baseUrl:
           "https://github.com/vscode-elements/vscode-elements.github.io/edit/main/",
